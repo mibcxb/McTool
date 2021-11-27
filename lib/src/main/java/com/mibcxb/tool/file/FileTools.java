d159c025295f3b65c34a31d63730354fca3717da
mibcxb <mibcxb@gmail.com>
2021-11-27 19:16:17 +0800

    add FileTools

---
+++ lib/src/main/java/com/mibcxb/tool/file/FileTools.java
@@ -0,0 +1,19 @@
+package com.mibcxb.tool.file;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Set;
+
+public class FileTools {
+    public void detectDuplicate(String path) {
+
+    }
+
+    public void detectDuplicate(File file) {
+
+    }
+
+    private void detectDuplicate(File file, Map<String, Set<String>> dupMap) {
+
+    }
+}
